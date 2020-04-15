d5a4da58cffa512c9eb5821ee625f6460704ffd8
Vreyes26 <63244722+Vreyes26@users.noreply.github.com>
2020-04-15 17:38:32 -0400

    Add files via upload

---
+++ ApertureScienceP/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>ApertureScience</title>
+        <link href="css/Style.css" type="text/css" rel="stylesheet">
+        <link rel="icon" href="Resouce/LogoAperture.png" type="image/png">
+    </head>
+    <header>
+        <img src="Resouce\ApertureScience2.png" alt="LogoApertureScience"/>
+        <p>Instalaciones secretas de investigacion 1953</p>
+    </header>
+    <body>
+        <div id="navegador">
+            <ul>
+                <li><a href="#info">Info</a></li>
+                <li><a href="#proyectos">Proyectos</a></li>
+                <li><a href="#donacion">Donacion</a></li>
+                <li><a href="#contacto">Contacto</a></li>
+            </ul>
+        </div>
+        <div id="texto">
+            <p>Aperture Science fue creada como Aperture Fixtures en los años 1940 por Cave Johnson.
+                Aperture Fixtures estaba principalmente dedicada a la creación y 
+                distribución de cortinas de baño– un portal de baja tecnología dentro y fuera de la ducha
+                – con Cave Johnson ganando el premio "Mejor vendedor de cortinas de baño de 1943".
+                Entre 1943 y 1947 el nombre fue cambiado a "Aperture Science Innovators". Aunque inicialmente se hizo para
+                que sus cortinas de baño sonaran más higiénicas, la compañía se empezaba a enfocar en la ciencia real.
+                Cave Johnson compró una grande, abandonada mina de sal en Michigan Norte donde el Centro de Desarrollo 
+                de Aperture Science se construiría.</p>
+            
+            <p>Entre los años 1940 y 1950, Aperture Science empezaría sus prácticas de investigación y pruebas con sujetos.
+                Los mejores de ellos eran de las Olimpiadas, astronautas o heroes de la guerra.
+                Fueron, además, el segundo mayor contratista después de Black Mesa para el Departamento de Defensa desde 1952 a 1954.
+                Los avances de Aperture en este periodo incluyeron Gel de Repulsión, el Cubo Contenedor Contrapesado, 
+                el Ultrabotón Industrial Extracolisionador de 1500 Megavatios y el Dispositivo de Tunelado Cuántico Portable 
+                de Aperture Science, una anterior y más grande versión de la moderna Pistola de Portales. </p>
+            
+            <img id="ImagenPG" src="Resouce/PistolaDePortales.jpg" alt="Imagen de pistola de portales">
+            
+            <p><aside>El Dispositivo Portátil de Portales de Aperture Science, también conocido comúnmente como Pistola de Portales
+                o por sus siglas en inglés, "ASHPD" (Aperture Science Handheld Portal Device), 
+                es una herramienta experimental utilizada para crear dos portales a través de la cual los objetos pueden pasar.</aside>
+                No importa la distancia entre ellos, cualquier objeto que pase a través de un portal saldrá del otro y viceversa 
+                instantáneamente. Los portales se pueden colocar en cualquier superficie que sea manufacturada o refinada
+                con roca lunar y sea lo suficientemente grande para acomodarse a ella.
+                Objetos, o ciertos tipos de superficies en movimiento, generalmente previenen la formación de un portal.</p>
+        </div>
+    </body>
+    <footer>
+        <p>Contacto: 9000 500 700</p>
+        <p>Correo: ApertureScience@aperturelab.com</p>
+    </footer>
+</html>
